fbc2f42bb52bcfdf44cbd82f891aee996508b592
hoonee.cho <rubber815@gmail.com>
2024-04-17 08:20:39 +0900

    [Unittest] added a ssd lba write/read test with BabyStepNand (Not mock) - conntected full stack (test -> SSD -> BabyStepNand)

--- SSDTest/nand_test.cpp
+++ SSDTest/nand_test.cpp
@@ -4,21 +4,29 @@
 
 #include <stdexcept>
 #include "../SSDProject/BabyStepNAND.cpp"
+#include "../SSDProject/SSD.cpp"
 
 using namespace testing;
 
-/*
-1. W 20 0x1289CDEF
-2. R 20 --> 0x1289CDEF
-3. R 19 --> 0x00000000
-4. W 10 0xFF1100AA
-5. R 10 --> 0xFF1100AA
-*/
+class SSDTestWithBabyStepNand : public testing::Test {
+protected:
+	void SetUp() override {
+		ssd_.selectNAND(&b_nand_);
+	}
+public:
+	SSD ssd_;
+	BabyStepNand b_nand_;
+};
 
 TEST(NANDTest, Nand_single_write_read_test) {
 	BabyStepNand bsn;
-	bsn.write(0x3, "0x333");
-	EXPECT_EQ("0x333", bsn.read(0x3));
+	bsn.write(0x3, "0x444");
+	EXPECT_EQ("0x444", bsn.read(0x3));
+}
+
+TEST_F(SSDTestWithBabyStepNand, Write_lba_test_with_BabyStep) {
+	ssd_.write(0x4, "0x00000003");
+	EXPECT_EQ("0x00000003",ssd_.read(0x4));
 }
 
 // TODO
