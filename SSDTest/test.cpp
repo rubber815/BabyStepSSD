7f83823f53ddc39f4b850d8cc52bc38d41fc831b
sunyeal <sunyeal.hong@samsung.com>
2024-04-16 15:26:09 +0900

    [Refactor] seperate private function for validation check

--- SSDTest/test.cpp
+++ SSDTest/test.cpp
@@ -33,55 +33,62 @@ public:
 	MockNAND m_nand_;
 };
 
-TEST_F(SSDTest, Read_lba_test) {
+TEST_F(SSDTest, Write_lba_test) {
 	EXPECT_CALL(m_nand_, write(0, "0x00000000"))
 		.Times(1);
 	ssd_.write(0, "0x00000000");
 }
 
-TEST_F(SSDTest, Read_lower_lba_fail_test) {
+TEST_F(SSDTest, Write_lower_lba_fail_test) {
 	EXPECT_CALL(m_nand_, write(-1, "0x00000000"))
 		.Times(0);
 
 	EXPECT_THROW(ssd_.write(-1, "0x00000000"), std::invalid_argument);
 }
 
-TEST_F(SSDTest, Read_upper_lba_fail_test) {
+TEST_F(SSDTest, Write_upper_lba_fail_test) {
 	EXPECT_CALL(m_nand_, write(101, "0x00000000"))
 		.Times(0);
 
 	EXPECT_THROW(ssd_.write(101, "0x00000000"), std::invalid_argument);
 }
 
-TEST_F(SSDTest, Read_data_out_of_rang_fail_test) {
+TEST_F(SSDTest, Write_data_out_of_rang_fail_test) {
 	EXPECT_CALL(m_nand_, write(0, "0x0000000000"))
 		.Times(0);
 
 	EXPECT_THROW(ssd_.write(0, "0x0000000000"), std::invalid_argument);
 }
 
-TEST_F(SSDTest, Read_data_prefix_fail_test) {
+TEST_F(SSDTest, Write_data_prefix_fail_test) {
 	EXPECT_CALL(m_nand_, write(0, "xx00000000"))
 		.Times(0);
 
 	EXPECT_THROW(ssd_.write(0, "xx00000000"), std::invalid_argument);
 }
 
-TEST_F(SSDTest, Write_test_normal_success) {
+TEST_F(SSDTest, Read_test_normal_success) {
 	int testLBA = 0;
 	EXPECT_CALL(m_nand_, read(testLBA))
 		.WillOnce(Return("0xAAAABBBB"));
 	EXPECT_THAT(ssd_.read(testLBA), Eq("0xAAAABBBB"));
 
-	int testLBA = 30;
+	testLBA = 30;
 	EXPECT_CALL(m_nand_, read(testLBA))
 		.WillOnce(Return("0xAAAABBBB"));
 	EXPECT_THAT(ssd_.read(testLBA), Eq("0xAAAABBBB"));
 }
 
-TEST_F(SSDTest, Write_test_boundary_check_fail) {
-	EXPECT_THAT(ssd_.read(-1), Eq("0x00000000")) << "Check invalid scope";
-	EXPECT_THAT(ssd_.read(100), Eq("0x00000000")) << "Check invalid scope";
+TEST_F(SSDTest, Read_test_boundary_check_fail) {
+	EXPECT_CALL(m_nand_, read(-1))
+		.Times(0);
+
+	EXPECT_THROW(ssd_.read(-1), std::invalid_argument);
+
+	EXPECT_CALL(m_nand_, read(100))
+		.Times(0);
+
+	EXPECT_THROW(ssd_.read(100), std::invalid_argument);
 }
 
 // TODO
