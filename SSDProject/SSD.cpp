bf97d7907e581fa20aa8ea890286cd41e3870e5b
sunyeal <sunyeal.hong@samsung.com>
2024-04-16 14:21:19 +0900

    [Feature] initial write function of SSD

--- SSDProject/SSD.cpp
+++ SSDProject/SSD.cpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../SSDProject/INAND.cpp"
+#include <stdexcept>
 
 class SSD {
 public:
@@ -9,6 +10,9 @@ public:
 	}
 	void write(int lba, std::string value) {
 		// TODO
+		if (lba < 0 || lba > 100)
+			throw std::invalid_argument("lba is incorrect");
+		nand_->write(lba, value);
 	}
 	std::string read(int lba) {
 		// TODO
