ac74184a9b1a3619a42af11d83f9441346eec253
hyunjun.kwon <hyunjun.kwon@samsung.com>
2024-04-22 16:30:38 +0900

    [Feature] Add new test case which is failed and decrease loop case

--- Write10AndCompareFail/main.cpp
+++ Write10AndCompareFail/main.cpp
@@ -33,13 +33,18 @@ void writeToTestResultFile(std::string value) {
 
 }
 
-bool Loop_ReadAndWriteCompare() {
+bool Write10AndCompare() {
 	const std::vector<std::string> inputs
 		= { "0xABCDEFAB" , "0xEFFFFFFF" , "0x12345678" ,
 		"0x12345673" , "0xEFFFFEFF" , "0xABCDCFAB" ,
 	"0xABCDEFAB", "0xDBCDEFAB", "0xABADEFAB" ,"0xAB0DEFAB" };
 
 	const std::vector<int> lbas = { 1, 7, 30, 50, 33, 55, 87, 90, 27, 4 };
+
+	const std::vector<std::string> inputsCompare
+		= { "0xABCDEFAB" , "0xEFFFFFFF" , "0x12345678" ,
+		"0x12345673" , "0xEFFFFEFF" , "0xABCDCFAB" ,
+	"0xABCDEFAB", "0xDBADEFAB", "0xABADFFAB" ,"0xAB0DEFAB" };
 
 	// Initial Write
 	for (int idx = 0; idx < 10; idx++) {
@@ -49,16 +54,14 @@ bool Loop_ReadAndWriteCompare() {
 		system(cmd.c_str());
 	}
 
-	for (int i = 0; i < 10; i++) {
-		for (int idx = 0; idx < 10; idx++) {
-			std::string cmd = "ssd R ";
-			cmd += std::to_string(lbas[idx]);
-			system(cmd.c_str());
+	for (int idx = 0; idx < 10; idx++) {
+		std::string cmd = "ssd R ";
+		cmd += std::to_string(lbas[idx]);
+		system(cmd.c_str());
 
-			std::string read_val = readFromSSDResultFile();
-			if (read_val != inputs[idx])
-				return false;
-		}
+		std::string read_val = readFromSSDResultFile();
+		if (read_val != inputsCompare[idx])
+			return false;
 	}
 
 	return true;
@@ -66,7 +69,7 @@ bool Loop_ReadAndWriteCompare() {
 
 
 int main(int argc, char* argv[]) {
-	bool ret = Loop_ReadAndWriteCompare();
+	bool ret = Write10AndCompare();
 	if (ret) {
 		writeToTestResultFile("Pass");
 	}
